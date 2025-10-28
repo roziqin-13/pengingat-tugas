7f384649fc25065ca8a759940912b6a0299afd48
roziqin-13 <roziqinkhoir54@gmail.com>
2025-10-28 08:31:07 +0700

    update PWA dengan splash screen dan ikon

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,34 @@
-const CACHE_NAME = "pengingat-tugas-v1";
-const urlsToCache = [
+const CACHE_NAME = "pengingat-tugas-v2";
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./123.png",
+  "./321.jpg"
 ];
 
-// install service worker
+// Saat pertama kali diinstall
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  console.log("✅ Service Worker: Terinstal");
 });
 
-// aktifkan cache saat offline
+// Saat offline, ambil dari cache
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+// Update cache jika ada versi baru
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
