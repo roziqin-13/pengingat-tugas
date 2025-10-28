157a3eb337cf71662c36a572e6a25f99633bb93a
roziqin-13 <roziqinkhoir54@gmail.com>
2025-10-29 00:56:41 +0700

    update index.html with delete+undo+animation

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,13 @@
-const CACHE_NAME = "pengingat-v4";
-const FILES_TO_CACHE = ["./", "./index.html", "./manifest.json", "./123.png"];
-
-self.addEventListener("install", (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
-
-// === NOTIFIKASI PERIODIK ===
-self.addEventListener("periodicsync", (event) => {
-  if (event.tag === "pengingat-tugas") {
-    showNotification();
-  }
-});
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow("./index.html")
+    caches.open('pengingat-cache-v3').then(cache => {
+      return cache.addAll(['/', '/index.html', '/manifest.json']);
+    })
   );
 });
 
-function showNotification() {
-  self.registration.showNotification("Pengingat Tugas 🕐", {
-    body: "Ojo Klalen Buka TugasMu Saiki!",
-    icon: "123.png",
-    badge: "123.png",
-  });
-}
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
